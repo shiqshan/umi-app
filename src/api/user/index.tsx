8c1a377ad663cdc6475137a43dbc35aabd35a2cf
shiqingshan <shiqingshan@flowpp.com>
2023-05-08 11:49:09 +0800

    格式化

--- src/api/user/index.tsx
+++ src/api/user/index.tsx
@@ -7,25 +7,25 @@ import { Fetch, FetchApi } from '@/api/util';
 const userBaseApi = 'api/user';
 
 export interface ILoginParams {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
 }
 
 export interface UserInfo {
-  id: string;
-  name: string;
-  address: string;
-  sex: string;
-  tel_number: string;
-  qq: string;
-  account: string;
-  password: string;
-  age: number;
-  id_sort: number;
+    id: string;
+    name: string;
+    address: string;
+    sex: string;
+    tel_number: string;
+    qq: string;
+    account: string;
+    password: string;
+    age: number;
+    id_sort: number;
 }
 
 export const user_api: {
-  login: FetchApi<ILoginParams, UserInfo>;
+    login: FetchApi<ILoginParams, UserInfo>;
 } = {
-  login: (body) => Fetch(`${userBaseApi}/login`, body),
+    login: (body) => Fetch(`${userBaseApi}/login`, body),
 };
